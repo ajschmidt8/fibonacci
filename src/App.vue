1e270d3348eafa07df39e5af0e12c41a2fbaa406
AJ <ajs4392@gmail.com>
2018-02-11 13:31:50 -0800

    initial push

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="ui container">
+		<h1>
+			Hello World!
+		</h1>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'app',
+	components: {}
+}
+</script>
+
+<style scoped lang="scss">
+	h1 {
+		color: red;
+	}
+</style>
